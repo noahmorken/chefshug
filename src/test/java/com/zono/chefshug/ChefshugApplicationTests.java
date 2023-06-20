b6d3bcde8698dedf01d12affb28f06647290fc20
pmorken <pmorkenx@gmail.com>
2023-06-19 18:23:32 -0600

    Initial commit

---
+++ src/test/java/com/zono/chefshug/ChefshugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zono.chefshug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefshugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
